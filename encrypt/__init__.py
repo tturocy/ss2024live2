3263f4579d1bb7169b2a4ae0461dbfed439d4ee6
Ted Turocy <ted.turocy@gmail.com>
2024-07-10 16:39:25 +0100

    Basic logic of encryption task.

--- encrypt/__init__.py
+++ encrypt/__init__.py
@@ -1,9 +1,13 @@
+import random
+
 from otree.api import (
     BaseConstants,
     BaseGroup,
     BasePlayer,
     BaseSubsession,
+    Currency,
     Page,
+    models,
 )
 
 doc = """
@@ -15,10 +19,17 @@ class C(BaseConstants):
     NAME_IN_URL = "encrypt"
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 2
+    PAYMENT_PER_CORRECT = Currency(0.10)
 
 
 class Subsession(BaseSubsession):
-    pass
+    payment_per_correct = models.CurrencyField()
+
+    def setup(self):
+        self.payment_per_correct = C.PAYMENT_PER_CORRECT
+        word = random.choice("AB")
+        for player in self.get_players():
+            player.word = word
 
 
 class Group(BaseGroup):
@@ -26,7 +37,21 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    word = models.StringField()
+    response = models.IntegerField()
+    is_correct = models.BooleanField()
+
+    def compute_outcome(self):
+        self.is_correct = (
+                (self.word == "A" and self.response == 1) or
+                (self.word == "B" and self.response == 2)
+        )
+        if self.is_correct:
+            self.payoff = self.subsession.payment_per_correct
+
+
+def creating_session(subsession):
+    subsession.setup()
 
 
 # PAGES
@@ -37,7 +62,15 @@ class Intro(Page):
 
 
 class Decision(Page):
-    pass
+    form_model = "player"
+
+    @staticmethod
+    def get_form_fields(player):
+        return ["response"]
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        player.compute_outcome()
 
 
 class Outcome(Page):
